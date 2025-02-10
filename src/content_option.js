f2d1eeb342b550e122dce0b9a6a1e0c882d8c294
jokerexe01 <mondalsuraj745@gmail.com>
2025-02-10 12:04:36 +0530

    intial commit

---
+++ src/content_option.js
@@ -0,0 +1,167 @@
+const logotext = "SURAJ";
+const meta = {
+    title: "Suraj Mondal",
+    description: "I am Suraj Mondal, a ethical hacking enthusiasist, bug hunter and a youtube content creator",
+};
+
+const introdata = {
+    title: "Suraj Mondal",
+    animated: {
+        first: "Bug Hunter",
+        second: "Capture The Flag(CTF) Player",
+        third: "Youtube Content Creator",
+    },
+    description: "I am Suraj Mondal, a ethical hacking enthusiasist, bug hunter and a youtube content creator",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I am Suraj Mondal, a ethical hacking enthusiasist, bug hunter and a youtube content creator",
+};
+const worktimeline = [{
+        jobtitle: "Bug Bounty Hunter",
+        where: "hackerone, bugcrowed etc.",
+        date: "2022"
+    },
+    {
+        jobtitle: "Content Creator",
+        where: "Youtube",
+        date: "2023",
+    },
+    {
+        jobtitle: "Bug Bounty instructor",
+        where: "Online",
+        date: "2024"
+    }
+
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Bash",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "C",
+        value: 90,
+    },
+    {
+        name: "Linux",
+        value: 95,
+    }
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_r3jy6sc",
+    YOUR_TEMPLATE_ID: "template_h2823x6",
+    YOUR_USER_ID: "P0wTUaoLub-RRRk8k",
+};
+
+const socialprofils = {
+    github: "https://github.com/jokerexe01",
+    // facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/suraj-mondal/",
+    twitter: "https://x.com/jokerexe001",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
